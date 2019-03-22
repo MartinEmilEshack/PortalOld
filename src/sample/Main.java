0d8098ea91c2c03960b73e47f20b6e311bcfd4d3
Martin Emil <mar10martinemil@gmail.com>
2019-03-23 01:12:14 +0200

    first edit to ui (test)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Portal");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(root);
         primaryStage.show();
     }
 
