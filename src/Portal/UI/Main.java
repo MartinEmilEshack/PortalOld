3c1cd84df0112f47785764435d2cf910cf94974a
Martin Emil <mar10martinemil@gmail.com>
2019-05-04 20:16:31 +0200

    Splash screen fxml file added

--- src/Portal/UI/Main.java
+++ src/Portal/UI/Main.java
@@ -4,13 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Scene root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene root = FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.centerOnScreen();
         primaryStage.setTitle("Portal");
         primaryStage.setScene(root);
         primaryStage.show();
@@ -20,5 +23,8 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 
 }
